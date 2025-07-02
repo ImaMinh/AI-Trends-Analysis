e433e4ad40100e5bfa7b696e561dc776e35e0253
ImaMinh <handucminh2022@gmail.com>
2025-07-02 23:13:46 +0700

    finishing the project

--- salary_analysis/location_vs_salary.py
+++ salary_analysis/location_vs_salary.py
@@ -3,46 +3,72 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
 
+# =========================================
+# ========= Read and Prepare Data =========
+# =========================================
+
 data = pd.read_csv('C:/Users/handu/Codes/Personal/Data Analysis Project/AI_Trend Analysis/ai_job_dataset_cleaned.csv')[['company_location', 'salary_usd']]
 
-print(data.sample(10))
+#print(data.sample(10))
 
 df_stats = data.groupby(['company_location'])['salary_usd'].agg(['mean', 'median', max])
 #print(df_stats)
 
 locations = data['company_location'].unique()
 
+middle_index = len(locations) // 2
+
 print(data['company_location'].value_counts().__len__())
 
-fig, axes = plt.subplots(2, 10, sharex=True, sharey=True) # dùng một trục y chung cho tất cả các subplot
+# ====================================================================================
+# ======= Inspect how salary distribution is across countries before comparing =======
+# ====================================================================================
+
+fig, axes = plt.subplots(2, 5, figsize=(3, 3), sharex=True, sharey=True) # dùng một trục y chung cho tất cả các subplot
 axes = axes.flatten()
 
 plt.ylabel('Distribution')
 
-for ax, t in zip(axes, locations):
+for ax, t in zip(axes, locations[:middle_index]):
     vals = data.loc[data['company_location']==t, 'salary_usd']
-    ax.hist(vals, bins=5, edgecolor='black', alpha=0.5)
+    ax.hist(vals, bins=20, edgecolor='black', alpha=0.5)
     ax.set_title(t)
-    #ax.set_xticklabels(list(np.arange(90000, 300000, 3)),rotation = 45)
     ax.set_xlabel("Salary")
 
-plt.title("Salary Distributions by Countries", weight="bold", loc='center', y=2.3)
-plt.tight_layout()
+
+fig, axes = plt.subplots(2, 5, figsize=(3, 3), sharex=True, sharey=True) # dùng một trục y chung cho tất cả các subplot
+axes = axes.flatten()
+
+for ax, t in zip(axes, locations[middle_index:]):
+    vals = data.loc[data['company_location']==t, 'salary_usd']
+    ax.hist(vals, bins=20, edgecolor='black', alpha=0.5)
+    ax.set_title(t)
+    ax.set_xlabel("Salary")
+
 plt.show()
 
-# --- Statistical ---
+# =====================================================================================================
+# ===== Use Kruskal Wallis to evaluate if there are significant difference between each countries =====
+# =====================================================================================================
+
 groups = [data.loc[data['company_location']==c,'salary_usd'].reset_index(drop=True) for c in locations] #type: ignore
 H, p = stats.kruskal(*groups)
 print(f"H={H}, p={p:.3e}")
 
 
-# 2. Compute median and bootstrap CI
-def bootstrap_ci(x, stat=np.median, B=2000, alpha=0.05):
+# =============================================================================================
+# ====== Using Bootstrap to Evaluate Sampling Distribution Median and Compare the Medians =====
+# =============================================================================================
+
+def bootstrap_ci(x, stat=np.median, number_of_repeats=2000, alpha=0.05):
+    
     boot_stats = np.array([
         stat(np.random.choice(x, size=len(x), replace=True))
-        for _ in range(B)
+        for _ in range(number_of_repeats) # lặp lại quy trình B lần 
     ])
+    
     lower, upper = np.percentile(boot_stats, [100*alpha/2, 100*(1-alpha/2)])
+    
     return lower, upper
 
 summary = (
